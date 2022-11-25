a7982ace19ba5134cfb5aa783f99df9cc4131f68
ChristopherMUFU <christopher.mufu@gmail.com>
2022-11-25 12:23:13 +0100

    Django configuration

---
+++ onp_analysis/onp_analysis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for onp_analysis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onp_analysis.settings')
+
+application = get_asgi_application()
